cdf4da66a7f6fd4e1337452ec7c71f899e982b26
vrroshni <vrroshni2001@gmail.com>
2024-07-28 17:00:02 +0530

    added siblings to tags with react

--- Inception/practice/App.js
+++ Inception/practice/App.js
@@ -2,21 +2,36 @@ const heading = React.createElement("h1", { id: "heading" }, "HELLO WORLD FROM R
 
 {/* <div id="parent">
     <div id="child">
-        <h1>Hi I am nested H1</h1>
+        <h1>Hi I am nested h1 tag</h1>
+        <h1>Hi I am Sibling of h1 tag</h1>
+    </div>
+    <div id="child2">
+        <h1>Hi I am nested h1 tag</h1>
+        <h1>Hi I am Sibling of h1 tag</h1>
     </div>
 </div> */}
 
 const nested = React.createElement(
     "div",
     { id: "parent" },
-    React.createElement(
-        "div",
-        { id: "child" },
-       [ 
-        React.createElement("h1", {}, "Hi I am nested h1 tag"),
-        React.createElement("h2", {}, "Hi I am Sibling of h1 tag"),
-       ]
-    )
+    [
+        React.createElement(
+            "div",
+            { id: "child1" },
+            [
+                React.createElement("h1", {}, "Hi I am nested h1 tag"),
+                React.createElement("h2", {}, "Hi I am Sibling of h1 tag"),
+            ]
+        ),
+        React.createElement(
+            "div",
+            { id: "child2" },
+            [
+                React.createElement("h1", {}, "Hi I am nested h1 tag"),
+                React.createElement("h2", {}, "Hi I am Sibling of h1 tag"),
+            ]
+        )
+    ]
 )
 
 console.log(nested)
