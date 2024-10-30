e953091bebc26248249cb526066f8cb69fafe6dc
vrroshni <vrroshni2001@gmail.com>
2024-10-30 17:51:20 +0530

    explored parcel

---
+++ Igniting Our App/practice/App.js
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
+const heading = React.createElement("h1", { id: "heading" }, "HELLO WORLD FROM REACT");
+
+{/* <div id="parent">
+    <div id="child">
+        <h1>Hi I am nested h1 tag</h1>
+        <h1>Hi I am Sibling of h1 tag</h1>
+    </div>
+    <div id="child2">
+        <h1>Hi I am nested h1 tag</h1>
+        <h1>Hi I am Sibling of h1 tag</h1>
+    </div>
+</div> */}
+
+const nested = React.createElement(
+    "div",
+    { id: "parent" },
+    [
+        React.createElement(
+            "div",
+            { id: "child1" },
+            [
+                React.createElement("h1", {}, "This is Namste React"),
+                React.createElement("h2", {}, "Hi I am Sibling of h1 tag"),
+            ]
+        ),
+        React.createElement(
+            "div",
+            { id: "child2" },
+            [
+                React.createElement("h1", {}, "Hi I am nested h1 tag"),
+                React.createElement("h2", {}, "Hi I am Sibling of h1 tag"),
+            ]
+        )
+    ]
+)
+
+console.log(nested)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(nested); 
